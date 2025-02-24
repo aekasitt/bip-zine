991ab02fb99f30c5e0ecf91343e59c21f3e0fcc9
Sitt Guruvanich <aekazitt@gmail.com>
2025-02-24 22:45:11 +0700

    feat: make booklet front-end app from unbound pages

---
+++ src/unbind.ts
@@ -0,0 +1,39 @@
+/* ~~/src/unbind.ts */
+
+// imports
+import { execSync } from 'node:child_process'
+import { promisify } from 'node:util'
+
+// enumerate
+type Zine = 'bips' | 'bips.th' | 'payjoin' | 'payjoin.th'
+
+let unbind = async () => {
+  let zine: Zine = process.argv.slice(2)[0] as Zine
+  if (!['bips', 'bips.th', 'payjoin', 'payjoin.th'].includes(zine)) {
+    console.error('Invalid zine selected')
+    return
+  }
+  let commands: string[] = [
+    `pdftoppm -jpeg -r 300 ${zine}.pdf -f 1 -l 1 -singlefile -x 0 -y 0 -W 1240 -H 1754 public/${zine}.8`,
+    `pdftoppm -jpeg -r 300 ${zine}.pdf -f 1 -l 1 -singlefile -x 1240 -y 0 -W 1240 -H 1754 public/${zine}.1`,
+    `pdftoppm -jpeg -r 300 ${zine}.pdf -f 1 -l 1 -singlefile -x 0 -y 1754 -W 1240 -H 1754 public/${zine}.6`,
+    `pdftoppm -jpeg -r 300 ${zine}.pdf -f 1 -l 1 -singlefile -x 1240 -y 1754 -W 1240 -H 1754 public/${zine}.3`,
+    `pdftoppm -jpeg -r 300 ${zine}.pdf -f 2 -l 2 -singlefile -x 0 -y 0 -W 1240 -H 1754 public/${zine}.2`,
+    `pdftoppm -jpeg -r 300 ${zine}.pdf -f 2 -l 2 -singlefile -x 1240 -y 0 -W 1240 -H 1754 public/${zine}.7`,
+    `pdftoppm -jpeg -r 300 ${zine}.pdf -f 2 -l 2 -singlefile -x 0 -y 1754 -W 1240 -H 1754 public/${zine}.4`,
+    `pdftoppm -jpeg -r 300 ${zine}.pdf -f 2 -l 2 -singlefile -x 1240 -y 1754 -W 1240 -H 1754 public/${zine}.5`,
+  ]
+  for (let i = 0; i < commands.length; i++) {
+    let [stdout, stderr] = execSync(commands[i])
+    if (!!stderr) console.error(stderr)
+    else if (!!stdout) {
+      console.log(stdout)
+    }
+  }
+  console.log(`Images of unbound ${zine}.pdf pages created under public/ directory`)
+}
+
+// run
+promisify(unbind)()
+  .then(process.exit(0))
+  .catch(process.exit(-1))
